8e49c2bb7090ec0b7bd8a6cf356db961632c8d51
axdanbol <daniel_bolin@yahoo.com>
2020-01-29 13:03:04 -0500

    Cleaned up the forest extraction and tested correctness

--- scripts/20-extract_forest.sh
+++ scripts/20-extract_forest.sh
@@ -2,7 +2,4 @@
 source constants.sh
 set -ev
 
-FOREST_OUTPUT="/extract_forest"
-
-mkdir -p $OUT/extract_forest
-python3 -msrc.extract_forest.extract_forest -i $OUT/layers -o $OUT/$FOREST_OUTPUT
+python3 -msrc.forests "$LAYERS_DIR" "$FORESTS_DIR"
