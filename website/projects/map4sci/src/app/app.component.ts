1cc35a0b309e6683206621d027fbcfa82c15880a
axdanbol <daniel_bolin@yahoo.com>
2021-02-04 15:44:37 -0500

    Added website project

---
+++ website/projects/map4sci/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'm4s-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'map4sci';
+}
