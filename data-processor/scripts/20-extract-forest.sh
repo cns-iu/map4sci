b72098dfc85794ce33715b0828811d97f397078b
axdanbol <daniel_bolin@yahoo.com>
2021-02-04 15:37:22 -0500

    Removed impred and other old code

--- data-processor/scripts/20-extract-forest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source constants.sh
-set -ev
-
-python3 -msrc.forests "$LAYERS_DIR" "$FORESTS_DIR"
--- data-processor/scripts/24-impred-remove-label-overlap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source constants.sh
-set -ev
-
-mkdir -p $OUT/impred
-
-java -jar "libs/impred/ImPredoverlapremoval.jar" --inputgraph=$OUT/impred/layer0.dot --edgeattraction=10 --nodenoderepulsion=10 --edgenoderepulsion=5 --iterations=20 --outputfile=$OUT/impred/layer0.dot
-python3 -msrc.property_fetcher $OUT/layers/layer0.dot $OUT/impred/layer0.dot
