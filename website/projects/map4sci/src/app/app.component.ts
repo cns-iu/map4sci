2b7f8e1243c918bdf1db2f3c91da9785f525c5b5
adphillips2017 <adphillips2017@users.noreply.github.com>
2021-05-06 21:45:13 -0400

    added new dataset, index.json, added functionality to pull in new data

--- website/projects/map4sci/src/app/app.component.ts
+++ website/projects/map4sci/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Any } from '@angular-ru/common/typings';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FeatureCollection } from 'geojson';
@@ -7,6 +8,27 @@ const EMPTY_FEATURES: FeatureCollection = {
   features: []
 };
 
+const EMPTY_DATASET = {
+  id: 'empty',
+  boundary: EMPTY_FEATURES,
+  cluster: EMPTY_FEATURES,
+  edges: EMPTY_FEATURES,
+  nodes: EMPTY_FEATURES
+}
+
+interface MapDataset {
+  boundary: FeatureCollection;
+  cluster: FeatureCollection;
+  edges: FeatureCollection;
+  nodes: FeatureCollection;
+  id: string;
+}
+
+interface MapDatasetDirectory {
+  [key: string]: MapDataset;
+  [index: number]: MapDataset;
+}
+
 @Component({
   selector: 'm4s-root',
   templateUrl: './app.component.html',
@@ -15,37 +37,46 @@ const EMPTY_FEATURES: FeatureCollection = {
 export class AppComponent implements OnInit {
   constructor(private readonly http: HttpClient) { }
 
-  testFiles: any = {
-    boundary: EMPTY_FEATURES,
-    cluster: EMPTY_FEATURES,
-    edges: EMPTY_FEATURES,
-    nodes: EMPTY_FEATURES
-  };
+  files: string[] = [
+    'boundary',
+    'cluster',
+    'edges',
+    'nodes'
+  ];
 
-  ngOnInit(): void {
-    this.getData();
+  dataset: Any = { };
+  datasetKeys: string[] = [];
+  currentDataset: MapDataset = EMPTY_DATASET;
+
+  async ngOnInit(): Promise<void> {
+    this.datasetKeys = await this.getMapData();
+    this.currentDataset = this.dataset[this.datasetKeys[0]];
   }
 
-  getData(): void {
-    console.log('getData()');
-    const files: string[] = [
-      'boundary',
-      'cluster',
-      'edges',
-      'nodes'
-    ];
+  async getMapData(): Promise<string[]> {
+    const { http, dataset, files } = this;
+    const keys: string[] = [];
 
-    files.forEach(file => {
-      // tslint:disable-next-line: deprecation
-      this.http.get(`assets/datasets/test/${file}.geojson`).subscribe(data => {
-        console.log('Adding ', file);
-        this.testFiles[file] = data;
+    // Fetch the index.json file
+    const datasetDirectory: any = await http.get('assets/datasets/index.json').toPromise();
+
+    // For each directory in the idnex.json file, go through and fetch the map data.
+    datasetDirectory.forEach((tempDataset: Any) => {
+      keys.push(tempDataset.id);
+      this.dataset[tempDataset.id] = { id: tempDataset.id };
+      files.forEach(async file => {
+        const url = `${tempDataset.dir}/${file}.geojson`;
+        // tslint:disable-next-line: deprecation
+        dataset[tempDataset.id][file] = await http.get(url).toPromise();
       });
     });
+
+    return keys;
   }
 
   showFiles(): void {
-    console.log('testFiles: ', this.testFiles);
+    console.log('dataset: ', this.dataset);
+    console.log('this: ', this);
   }
 }
 
