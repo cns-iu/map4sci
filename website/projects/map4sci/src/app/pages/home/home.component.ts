fa4f66111fd352a6329c2194fe70a3fea46608fc
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-10-06 18:33:34 -0400

    Fix visualizer image position (only works first time page is loaded)

--- website/projects/map4sci/src/app/pages/home/home.component.ts
+++ website/projects/map4sci/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'm4s-home',
@@ -6,6 +7,16 @@ import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   @HostBinding('class') readonly clsName = 'm4s-home';
+
+  @ViewChild('visualizer', { read: ElementRef }) visualizer: ElementRef<HTMLElement>;
+  @ViewChild('markdown', { read: ElementRef }) markdown: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    window.addEventListener('load', () => {
+      const { visualizer, markdown } = this;
+      markdown.nativeElement.insertBefore(visualizer.nativeElement, markdown.nativeElement.children[1]);
+    });
+  }
 }
