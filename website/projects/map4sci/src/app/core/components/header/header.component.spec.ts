8ded74cd612f565ae78f7cc3b38b887ac1143ff0
axdanbol <axbolin@iu.edu>
2021-10-06 15:05:22 -0400

    Docs and tests for header component

---
+++ website/projects/map4sci/src/app/core/components/header/header.component.spec.ts
@@ -0,0 +1,67 @@
+import { Shallow } from 'shallow-render';
+
+import { SiteConfigurationService } from '../../services/site-configuration/site-configuration.service';
+import { HeaderComponent, LogoConfig } from './header.component';
+import { HeaderModule } from './header.module';
+
+
+describe('HeaderComponent', () => {
+  const sampleUrl = '/does/not/exist';
+
+  let configSpy: jasmine.SpyObj<SiteConfigurationService>;
+  let shallow: Shallow<HeaderComponent>;
+
+  function setLogoConfig(config: LogoConfig): void {
+    configSpy.get.and.returnValue(config);
+  }
+
+  beforeEach(() => {
+    configSpy = jasmine.createSpyObj('SiteConfigurationService', ['get']);
+    shallow = new Shallow(HeaderComponent, HeaderModule)
+      .mock(SiteConfigurationService, configSpy);
+  });
+
+  describe('.reverseLogoLayout', () => {
+    it('uses the configuration to compute the value', async () => {
+      setLogoConfig({ layout: 'label-first' });
+      const { instance } = await shallow.render();
+      expect(instance.reverseLogoLayout).toBeTrue();
+    });
+
+    it('is case insensitive to the configuration value', async () => {
+      setLogoConfig({ layout: 'LABEL-first' as 'label-first' });
+      const { instance } = await shallow.render();
+      expect(instance.reverseLogoLayout).toBeTrue();
+    });
+
+    it('defaults to false', async () => {
+      const { instance } = await shallow.render();
+      expect(instance.reverseLogoLayout).toBeFalse();
+    });
+  });
+
+  describe('.logoIcon', () => {
+    it('is loaded from the configuration', async () => {
+      setLogoConfig({ icon: sampleUrl });
+      const { instance } = await shallow.render();
+      expect(instance.logoIcon).toEqual(sampleUrl);
+    });
+  });
+
+  describe('.logoLabel', () => {
+    it('is loaded from the configuration', async () => {
+      const label = 'A Nice Company';
+      setLogoConfig({ label });
+      const { instance } = await shallow.render();
+      expect(instance.logoLabel).toEqual(label);
+    });
+  });
+
+  describe('.logoLink', () => {
+    it('is loaded from the configuration', async () => {
+      setLogoConfig({ linkTo: sampleUrl });
+      const { instance } = await shallow.render();
+      expect(instance.logoLink).toEqual(sampleUrl);
+    });
+  });
+});
--- website/projects/map4sci/src/app/core/components/header/header.component.ts
+++ website/projects/map4sci/src/app/core/components/header/header.component.ts
@@ -3,22 +3,38 @@ import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { SiteConfigurationService } from '../../services/site-configuration/site-configuration.service';
 
 
-interface LogoConfig {
+/**
+ * Logo configuration.
+ */
+export interface LogoConfig {
+  /** Whether to put the icon or text first. */
   layout?: 'icon-first' | 'label-first';
+  /** Url for an icon. */
   icon?: string;
+  /** Logo label. */
   label?: string;
+  /** External link. */
   linkTo?: string;
 }
 
-interface HeaderConfig {
+/**
+ * Header configuration.
+ */
+export interface HeaderConfig {
   logo?: LogoConfig;
 }
 
-interface SiteConfigurationWithHeaderConfig {
+/**
+ * Partial site configuration containing the header configuration.
+ */
+export interface SiteConfigurationWithHeaderConfig {
   header?: HeaderConfig;
 }
 
 
+/**
+ * Page header component.
+ */
 @Component({
   selector: 'm4s-header',
   templateUrl: './header.component.html',
@@ -26,13 +42,23 @@ interface SiteConfigurationWithHeaderConfig {
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
+  /** Component css selector. */
   @HostBinding('class') readonly clsName = 'm4s-header';
 
+  /** Whether to reverse the content of the logo so that the label is put first. */
   readonly reverseLogoLayout: boolean;
+  /** Logo icon url. */
   readonly logoIcon: string | undefined;
+  /** Logo label text. */
   readonly logoLabel: string;
+  /** External link to open on logo click. */
   readonly logoLink: string;
 
+  /**
+   * Creates an instance of header component.
+   *
+   * @param siteConfig The loaded site configuration.
+   */
   constructor(siteConfig: SiteConfigurationService<SiteConfigurationWithHeaderConfig>) {
     const { layout, icon, label = '', linkTo = '' } = siteConfig.get('header', 'logo') ?? {};
 
