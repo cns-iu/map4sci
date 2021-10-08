f7d7901f7a0e058067f56b1aa65c46491b7970a5
Edward Lu <36934022+edlu77@users.noreply.github.com>
2021-10-08 18:08:59 -0400

    Pbi home page fixes (#52)
    * Add google analytics tracking; change header home link to routing
    
    * Change tracking state to non-ngx version; styling adjustments
    
    * Add Opt Int/Out button to footer
    
    * Remove unneeded code
    
    * Fix visualizer image position (only works first time page is loaded)
    
    * Change to correct assets; add myAURA description
    
    * Styling
    
    * Linting; styling for drawer toggle button
    
    * Merge in markdown image fixes
    
    * Missing lines

--- website/projects/map4sci/src/app/pages/home/home.component.ts
+++ website/projects/map4sci/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'm4s-home',
@@ -8,4 +9,16 @@ import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 })
 export class HomeComponent {
   @HostBinding('class') readonly clsName = 'm4s-home';
+
+  @ViewChild('markdown', { read: ElementRef }) markdown: ElementRef<HTMLElement>;
+  @ViewChild('visualizer', { read: ElementRef }) visualizer: ElementRef<HTMLElement>;
+
+  attachVisualizer(): void {
+    const {
+      visualizer: { nativeElement: visualizer },
+      markdown: { nativeElement: markdown }
+    } = this;
+    const anchor = markdown.querySelector('.visualizer-anchor');
+    markdown.insertBefore(visualizer, anchor);
+  }
 }
