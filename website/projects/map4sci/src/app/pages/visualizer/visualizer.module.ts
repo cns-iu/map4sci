b028d6989c4699e56159f9fccb51abcd1a3355cb
Adam Phillips <adphillips2017@users.noreply.github.com>
2021-10-08 12:14:40 -0400

    Feature: node search (#43 #47)
    * new search bar groundwork
    
    * refactored to place map pins instead of filtering nodes.
    
    * clean up. style fixes.
    
    * working except map pins don't get removed on clear
    
    * Remove markers on clear and new search term; show starting dataset on load
    
    * Fix linting
    
    * Rerun search when dataset changes
    
    Co-authored-by: Edward Lu <36934022+edlu77@users.noreply.github.com>

--- website/projects/map4sci/src/app/pages/visualizer/visualizer.module.ts
+++ website/projects/map4sci/src/app/pages/visualizer/visualizer.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MapDataService } from '../../services/map-data.service';
-import { MapModule } from '../../map/map.module';
-import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-
-import { VisualizerRoutingModule } from './visualizer-routing.module';
-import { VisualizerComponent } from './visualizer.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
+import { MapModule } from '../../map/map.module';
+import { MapDataService } from '../../services/map-data.service';
+import { VisualizerRoutingModule } from './visualizer-routing.module';
+import { VisualizerComponent } from './visualizer.component';
 
 @NgModule({
   imports: [
@@ -20,7 +23,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatFormFieldModule,
     MatIconModule,
     MapModule,
-    MatSidenavModule
+    MatSidenavModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   declarations: [VisualizerComponent],
   providers: [MapDataService]
