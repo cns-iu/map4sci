2c48d7fed575931b07405bd24d2634badd730d20
Edward Lu <36934022+edlu77@users.noreply.github.com>
2022-01-14 14:40:34 -0500

    Change styling

--- website/projects/map4sci/src/app/shared/components/network/network.component.ts
+++ website/projects/map4sci/src/app/shared/components/network/network.component.ts
@@ -2,22 +2,31 @@ import {
   ChangeDetectionStrategy,
   Component,
   ElementRef,
+  EventEmitter,
   HostBinding,
   Input,
   OnChanges,
   OnDestroy,
-  SimpleChanges,
-  EventEmitter,
   Output,
-  OnInit
+  SimpleChanges,
 } from '@angular/core';
-import cytoscape, { Core as Cytoscape, EdgeDataDefinition, EdgeDefinition, NodeCollection, NodeDataDefinition, NodeDefinition, Singular, Stylesheet } from 'cytoscape';
-import { Node, Edge } from 'app/map/map';
+import cytoscape, {
+  Core as Cytoscape,
+  EdgeDataDefinition,
+  EdgeDefinition,
+  NodeCollection,
+  NodeDataDefinition,
+  NodeDefinition,
+  Singular,
+  Stylesheet,
+} from 'cytoscape';
+
+import { Edge, Node } from '../../../map/map';
 
 const nodeConfig: Node[] = [
-  { level: 0, zoom: 0.0, fontSize: 10000 },
-  { level: 1, zoom: 0.0, fontSize: 10000 },
-  { level: 2, zoom: 0.004, fontSize: 5000 },
+  { level: 0, zoom: 0.0, fontSize: 6000 },
+  { level: 1, zoom: 0.0, fontSize: 6000 },
+  { level: 2, zoom: 0.004, fontSize: 4000 },
   { level: 3, zoom: 0.008, fontSize: 2000 },
   { level: 4, zoom: 0.012, fontSize: 1500 },
   { level: 5, zoom: 0.016, fontSize: 1000 },
@@ -28,16 +37,16 @@ const nodeConfig: Node[] = [
 ];
 
 const edgeConfig: Edge[] = [
-  { level: 0, zoom: 0, color: 'blue', width: 1000, opacity: 0.0, borderOpacity: 0.0, borderColor: '', borderWidth: 0 },
-  { level: 1, zoom: 0.0, color: 'blue', width: 800, opacity: 1.0, borderOpacity: 1.0, borderColor: 'yellow', borderWidth: 1.2 },
-  { level: 2, zoom: 0.004, color: 'red', width: 700, opacity: 1.0, borderOpacity: 1.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 3, zoom: 0.008, color: 'orange', width: 600, opacity: 0.9, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 4, zoom: 0.012, color: '#c1b276', width: 500, opacity: 0.9, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 5, zoom: 0.016, color: '#94895f', width: 400, opacity: 0.8, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 6, zoom: 0.024, color: '#615b43', width: 300, opacity: 0.8, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 7, zoom: 0.028, color: '#c0c0c0', width: 200, opacity: 0.7, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 8, zoom: 0.032, color: '#c0c0c0', width: 200, opacity: 0.6, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 },
-  { level: 9, zoom: 0.036, color: '#c0c0c0', width: 200, opacity: 0.5, borderOpacity: 0.0, borderColor: '#F9D776', borderWidth: 1 }
+  { level: 0, zoom: 0, color: '#FFEBA1', width: 1000, opacity: 0.0 },
+  { level: 1, zoom: 0.0, color: '#FFEBA1', width: 800, opacity: 1.0 },
+  { level: 2, zoom: 0.004, color: '#FFEBA1', width: 700, opacity: 1.0 },
+  { level: 3, zoom: 0.008, color: '#F9D776', width: 600, opacity: 0.9 },
+  { level: 4, zoom: 0.012, color: '#c1b276', width: 500, opacity: 0.9 },
+  { level: 5, zoom: 0.016, color: '#94895f', width: 400, opacity: 0.8 },
+  { level: 6, zoom: 0.024, color: '#615b43', width: 300, opacity: 0.8 },
+  { level: 7, zoom: 0.028, color: 'gray', width: 200, opacity: 0.7 },
+  { level: 8, zoom: 0.032, color: 'gray', width: 200, opacity: 0.6 },
+  { level: 9, zoom: 0.036, color: 'gray', width: 200, opacity: 0.5 }
 ];
 
 @Component({
@@ -46,7 +55,7 @@ const edgeConfig: Edge[] = [
   styleUrls: ['./network.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NetworkComponent implements OnInit, OnChanges, OnDestroy {
+export class NetworkComponent implements OnChanges, OnDestroy {
   @HostBinding('class') readonly clsName = 'm4s-network';
 
   @Input() nodes: NodeDefinition[];
@@ -57,7 +66,7 @@ export class NetworkComponent implements OnInit, OnChanges, OnDestroy {
 
   private cy?: Cytoscape;
 
-  private zoom = 0;
+  private zoom: number;
 
   allNodes: NodeCollection;
 
@@ -66,32 +75,28 @@ export class NetworkComponent implements OnInit, OnChanges, OnDestroy {
 
   constructor(private readonly el: ElementRef) { }
 
-  ngOnInit(): void {
-    this.cy = this.createNetwork();
-    this.cy.elements('node[level = 1]').addClass(`label-${this.nodeZoomIndex}`).addClass('label-visible');
-    this.cy.elements('edge[level = 1]').addClass(`edge-${this.edgeZoomIndex}`);
-    this.attachListeners();
-    this.allNodes = this.cy.filter(element => {return element.isNode()})
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('nodes' in changes || 'edges' in changes) {
+      this.destroyNetwork();
+      this.cy = this.createNetwork();
+      this.cy.zoom(.002);
+      this.cy.elements('node[level <= 1]').addClass(`label-${this.nodeZoomIndex}`).addClass('label-visible');
+      this.cy.elements('edge[level <= 1]').addClass(`edge-${this.edgeZoomIndex}`);
+      this.cy.elements('edge[level > 1]').addClass('edge');
+      this.attachListeners();
+      this.allNodes = this.cy.filter(element => element.isNode());
+    }
   }
 
   ngOnDestroy(): void {
     this.destroyNetwork();
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if ('nodes' in changes || 'edges' in changes) {
-      this.destroyNetwork();
-      this.cy = this.createNetwork();
-      this.attachListeners();
-    }
-  }
-
   createStylesheet(): Stylesheet[] {
-    let styles: Stylesheet[] = [
+    const styles: Stylesheet[] = [
       {
         selector: 'node',
         style: {
-          'label': '',
           'height': 2000,
           'width': 2000,
           'backgroundColor': 'black'
@@ -107,26 +112,25 @@ export class NetworkComponent implements OnInit, OnChanges, OnDestroy {
       {
         selector: '.label-visible',
         style: {
-          'label': 'data(label)'
+          'label': 'data(label)',
+          'color': 'white'
         }
       }
     ];
-    for (var node of nodeConfig) {
+    for (const node of nodeConfig) {
       styles.push({
         selector: `.label-${node.level}`,
         style: {
           'font-size': nodeConfig[node.level].fontSize
         }
-      })
-    };
-    for (var edge of edgeConfig) {
+      });
       styles.push({
-        selector: `.edge-${edge.level}`,
+        selector: `.edge-${node.level}`,
         style: {
-          'width': edgeConfig[edge.level].width,
-          'line-color': edgeConfig[edge.level].color,
+          'width': edgeConfig[node.level].width,
+          'line-color': edgeConfig[node.level].color
         }
-      })
+      });
     }
     return styles;
   }
@@ -163,18 +167,20 @@ export class NetworkComponent implements OnInit, OnChanges, OnDestroy {
       });
 
       cy.on('zoom', () => {
-        let z = cy.zoom();
+        const z = cy.zoom();
         this.zoom = z;
         const oldNodeIndex = this.nodeZoomIndex;
         const oldEdgeIndex = this.edgeZoomIndex;
+        cy.startBatch();
         if (this.nodeLevelChange()) {
-          cy.elements(`node[level <= ${nodeConfig[this.nodeZoomIndex].level}]`).removeClass(`label-${oldNodeIndex}`).addClass(`label-${this.nodeZoomIndex}`).addClass('label-visible');
-          cy.elements(`node[level > ${nodeConfig[this.nodeZoomIndex].level}]`).removeClass('label-visible');
+          cy.$(`node[level <= ${this.nodeZoomIndex}]`).removeClass(`label-${oldNodeIndex}`).addClass(`label-${this.nodeZoomIndex}`).addClass('label-visible');
+          cy.$(`node[level > ${this.nodeZoomIndex}]`).removeClass('label-visible');
         }
         if (this.edgeLevelChange()) {
-          cy.elements(`edge[level = ${edgeConfig[this.edgeZoomIndex].level}]`).removeClass('edge').addClass(`edge-${this.edgeZoomIndex}`)
-          cy.elements(`edge[level > ${edgeConfig[this.edgeZoomIndex].level}]`).removeClass(`edge-${oldEdgeIndex}`).addClass('edge');
+          cy.$(`edge[level = ${this.edgeZoomIndex}]`).addClass(`edge-${this.edgeZoomIndex}`);
+          cy.$(`edge[level > ${this.edgeZoomIndex}]`).removeClass(`edge-${oldEdgeIndex}`);
         }
+        cy.endBatch();
       });
     }
   }
