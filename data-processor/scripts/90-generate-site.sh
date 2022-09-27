1e2dfa5b6fed2bad0f4ea6af2fee4141bd24994b
Bruce W. Herr II <bherr@indiana.edu>
2022-09-27 14:23:33 -0400

    Updates to docker build

--- data-processor/scripts/90-generate-site.sh
+++ data-processor/scripts/90-generate-site.sh
@@ -11,8 +11,9 @@ cp $OUT/clustered/*.geojson $OUT_DS
 rm -f $OUT_DS/*.cx
 cp $OUT/clustered/*.cx $OUT_DS
 
-rm -rf site
-cp -r ../website/dist/map4sci/ site
+mkdir -p site
+rm -rf site/*
+cp -r ../website/dist/map4sci/* site
 cp -r $OUT/site-data/* site/assets
 
 if [ ! -e $OUT/site-data/datasets/index.json ]; then
