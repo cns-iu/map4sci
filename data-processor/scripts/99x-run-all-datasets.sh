e50aa6dd033a82d1227d27b157a06291ce2bac7d
Bruce W. Herr II <bherr@indiana.edu>
2023-04-21 13:41:38 -0400

    P -> p :-(

--- data-processor/scripts/99x-run-all-datasets.sh
+++ data-processor/scripts/99x-run-all-datasets.sh
@@ -8,7 +8,7 @@ do
   OUTPUT_DIR="$RAW_DATA_DIR/$dataset/$VERSION"
   if [ ! -e $OUTPUT_DIR/map4sci-completed ]
   then
-    mkdir -P $OUTPUT_DIR
+    mkdir -p $OUTPUT_DIR
     export CURRENT_DATASET=$dataset
     ./run.sh $dataset &> $OUTPUT_DIR/map4sci.log.txt
     touch $OUTPUT_DIR/map4sci-completed
